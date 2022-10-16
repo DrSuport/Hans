7aed4ef160dc50f3df4f83bc2eca4e49cd012c2a
Warmek <bartek.sto14rczyk@gmail.com>
2022-10-16 21:21:06 +0200

    Implemented queue

--- src/main/java/commands/loop.java
+++ src/main/java/commands/loop.java
@@ -1,12 +1,14 @@
 package commands;
 
+import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
+import com.sedmelluq.discord.lavaplayer.track.AudioTrackEndReason;
+import lavaplayer.GuildMusicManager;
 import lavaplayer.PlayerManager;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.interactions.commands.build.OptionData;
 
 
 public class loop extends ACommand{
-    private static PlayerManager INSTANCE;
     public static String name = "loop";
     public static String description = "Loop's currently playing song";
     public static String access = null;
@@ -23,10 +25,4 @@ public class loop extends ACommand{
 
     }
 
-    public static PlayerManager getINSTANCE(){
-        if(INSTANCE == null){
-            INSTANCE = new PlayerManager();
-        }
-        return INSTANCE;
-    }
 }
--- src/main/java/commands/queue.java
+++ src/main/java/commands/queue.java
@@ -1,16 +1,22 @@
 package commands;
 
 
+import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
+import lavaplayer.GuildMusicManager;
 import lavaplayer.PlayerManager;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.interactions.commands.OptionMapping;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.OptionData;
+
+import java.util.ArrayList;
 
 
 public class queue extends ACommand{
     public static String name = "queue";
     public static String description = "Shows queue";
     public static String access = null;
-    public static OptionData option = null;
+    public static OptionData option = new OptionData(OptionType.INTEGER, "page", "Which page should i display", false);
 
 
 
@@ -20,6 +26,40 @@ public class queue extends ACommand{
 
     @Override
     public void Execute(SlashCommandInteractionEvent event) { //TODO queue execute
+        final GuildMusicManager musicManager = PlayerManager.getINSTANCE().getMusicManager(event.getGuild());
 
+        StringBuilder response = new StringBuilder();
+
+        int page;
+
+        OptionMapping messageOption = event.getOption("page");
+        if(messageOption != null){
+            page = messageOption.getAsInt();
+        }else{
+            page = 1;
+        }
+
+        page-=1;
+
+        int start = 20*page;
+        int end = 20*(page+1);
+
+        ArrayList<AudioTrack> audioTracks = new ArrayList<>();
+
+        musicManager.scheduler.queue.drainTo(audioTracks);
+        musicManager.scheduler.queue.addAll(audioTracks);
+
+
+        for (int i = start; i < end; i++) {
+            AudioTrack track = audioTracks.get(i);
+            response.append(++i).append(". **'").append(track.getInfo().title).append("'** by **'").append(track.getInfo().author).append("'**\n");
+        }
+
+        final String out = response.toString();
+
+        event.reply("").setEphemeral(false) // reply or acknowledge
+                .flatMap(v ->
+                        event.getHook().editOriginalFormat(out)
+                ).queue();
     }
 }
